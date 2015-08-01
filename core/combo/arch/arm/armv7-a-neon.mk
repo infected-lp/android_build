6fd91eeb1a381bad5a644cbfd447ad10eefd668e
Paul Beeler <pbeeler80@gmail.com>
2015-08-01 01:27:18 -0400

    [arm/arm64] Add a option to use toolchain defaults:
    This allows a option to be set for toolchains that handle the
    cortex and neon flags by default to be used instead of ugly rom hacks
    in armv7-a-neon.mk.  This is especially usefull for devices like
    flounder that use two different arm toolchains for 32 and 64bit binaries.
    
    Change-Id: I10808f704c9c6874db6af6a2fb8bd6a1c70202a8
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>
    
    Conflicts:
    	core/combo/arch/arm/armv7-a-neon.mk

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -6,6 +6,8 @@ ARCH_ARM_HAVE_VFP               := true
 ARCH_ARM_HAVE_VFP_D32           := true
 ARCH_ARM_HAVE_NEON              := true
 
+# If you're using toolchains that handle cortex and neon flags by default, set this to true.
+ifneq ($(strip $(USE_GCC_DEFAULTS)),true)
 CORTEX_A15_TYPE := \
 	cortex-a15 \
 	krait \
@@ -19,22 +21,31 @@ ifneq (,$(filter $(CORTEX_A15_TYPE),$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT
 	arch_variant_cflags := -mcpu=cortex-a15
 else
 ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a9)
-	arch_variant_cflags := -mcpu=cortex-a9
+	arch_variant_cflags := -mcpu=cortex-a9 -mfpu=neon
+	arch_variant_ldflags := \
+		-Wl,--no-fix-cortex-a8
 else
 ifneq (,$(filter cortex-a8 scorpion,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
-	arch_variant_cflags := -mcpu=cortex-a8
+	arch_variant_cflags := -mcpu=cortex-a8 -mfpu=neon
 	arch_variant_ldflags := \
 		-Wl,--fix-cortex-a8
 else
 ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a7)
-	arch_variant_cflags := -mcpu=cortex-a7
+	arch_variant_cflags := -mcpu=cortex-a7 -mfpu=neon-vfpv4
 	arch_variant_ldflags := \
 		-Wl,--no-fix-cortex-a8
 else
-	arch_variant_cflags := -march=armv7-a
+ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a5)
+	arch_variant_cflags := -mcpu=cortex-a7 -mfpu=neon-vfpv4
+	arch_variant_ldflags := \
+		-Wl,--no-fix-cortex-a8
+else
+	arch_variant_cflags := -march=armv7-a -mfpu=neon
 	# Generic ARM might be a Cortex A8 -- better safe than sorry
 	arch_variant_ldflags := \
 		-Wl,--fix-cortex-a8
+endif
+endif
 endif
 endif
 endif
